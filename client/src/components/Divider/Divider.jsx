1b8b3f438ac7287462ac335674033de7a0caa81c
Stefan Hadzhiev <stefan.hadzhiev@sap.com>
2022-05-31 21:30:04 +0300

    Divider component styles for light/dark theme

--- client/src/components/Divider/Divider.jsx
+++ client/src/components/Divider/Divider.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import classes from './styles.module.css';
+import { useAppContext } from '../../context/AppContext';
 
 const Divider = ({ style }) => {
-    return <div style={style} className={classes.divider}></div>;
+    const {
+        applicationState: { theme },
+    } = useAppContext();
+
+    return (
+        <div
+            style={style}
+            className={
+                theme.light
+                    ? `${classes.divider} ${classes['divider-light-theme']}`
+                    : `${classes.divider} ${classes['divider-dark-theme']}`
+            }
+        ></div>
+    );
 };
 
 export default Divider;
